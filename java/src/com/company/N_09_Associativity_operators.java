558b7ec72f780827dcf57861878fbd9eefe4f479
Arya bardhan das <dasarya2002@gmail.com>
2023-02-28 23:23:41 +0530

    Initial commit

---
+++ java/src/com/company/N_09_Associativity_operators.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class N_09_Associativity_operators {
+    public static void main(String[] args) {
+        int a = 6 * 5 - 34/2;
+        // precedence
+        
+        int b = 6 / 5 - 34*2;
+        System.out.println(a);
+//        Associativity
+        
+    }
+}
